e6d589b954194b571cf46ced8b6377bc0223d2fb
maneeshaperera99 <mansishaamarasinghe946@gmail.com>
2022-05-05 00:25:05 +0530

    Fourth update

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ function PageTransitions(){
             element.classList.add('active');
         }
     })
+
+ //Toggle theme
+ const themeBtn =document. querySelector('.theme-btn');
+ themeBtn.addEventListener('click',() =>{
+     let element = document.body;
+     element.classList.toggle('light-mode')
+ })  
 }
 
 PageTransitions();
